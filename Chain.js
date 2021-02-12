10e04730b74638c18047f5f42f7bae5511d01a97
avni-123 <sumedha.bhasin@gmail.com>
2021-02-12 18:52:45 +0530

    angry birds

--- Chain.js
+++ Chain.js
@@ -1,20 +1,33 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
+
+        this.pointB = pointB;
+
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
+    birdFly(){
+
+        this.chain.bodyA = null;
+
+    }
+
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+        if(this.chain.bodyA){
+            var A = this.chain.bodyA.position;
+            var B = this.pointB;
+            strokeWeight(4);
+            line(A.x, A.y, B.x, B.y);
+        }
+
     }
     
 }
